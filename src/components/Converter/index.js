b46f9e1fbc73adb08fb55a313469a63e62ea11c1
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2020-05-18 19:38:47 -0300

    Form done using unform and Yup validation.

---
+++ src/components/Converter/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Container } from './styles';
+import * as Yup from 'yup';
+import { Form, Input } from '@rocketseat/unform';
+
+const schema = Yup.object().shape({
+  binary: Yup.string('The value must be a sequence of zeros and ones.')
+    .matches(/^[01]+$/, 'The value must be binary.')
+    .required('The value must be a sequence of zeros and ones.'),
+});
+
+function Converter() {
+  function handleSubmit(data) {}
+  return (
+    <Container>
+      <h1>Binary to Decimal Converter</h1>
+      <p>Type only 8 characters using only 1 and 0.</p>
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="binary" maxLength={8} placeholder="Ex: 1001001" />
+        <button type="submit">Convert</button>
+      </Form>
+    </Container>
+  );
+}
+
+export default Converter;
---
+++ src/components/Converter/styles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #ffff;
+  padding: 20px;
+  /* box-shadow: 0px 0px 5px 3px rgba(219, 219, 219, 0.35); */
+  height: 300px;
+  width: 100%;
+  max-width: 360px;
+  border-radius: 4px;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    color: #3d3d3d;
+    font-size: 26px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    color: #858585;
+    margin-bottom: 10px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    input {
+      height: 56px;
+      border: 0;
+      border-radius: 4px;
+      margin-bottom: 5px;
+      background: #85858530;
+
+      font-size: 16px;
+      padding-left: 15px;
+    }
+
+    button {
+      height: 56px;
+      border-radius: 4px;
+      border: 0;
+      background: #5a33c8;
+      color: #fff;
+      font-size: 16px;
+      font-weight: bold;
+    }
+  }
+`;
